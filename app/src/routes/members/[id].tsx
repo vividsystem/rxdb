82f83695bc560362ddf784e25e43d9d4f8973d80
vividsystem <notrealbutnotobot@gmail.com>
2025-05-06 21:13:12 +0200

    fix: only accept numbers as parameter

--- app/src/routes/members/[id].tsx
+++ app/src/routes/members/[id].tsx
@@ -1,18 +1,22 @@
-import { createAsync, RouteDefinition, useNavigate, useParams } from "@solidjs/router";
-import { Show } from "solid-js";
+import { createAsync, redirect, RouteDefinition, useNavigate, useParams } from "@solidjs/router";
+import { onMount, Show } from "solid-js";
 import { getMember, getMembers } from "~/api/";
 import { MemberEditPopUp } from "~/components/MemberPopUp";
 
 
 export const route = {
   preload() {
-    getMembers();
   }
 } satisfies RouteDefinition;
+
+
 
 export default function MemberPage() {
 	const navigate = useNavigate()
 	const params = useParams();
+	if(isNaN((parseInt(params.id, 10)))) {
+		return navigate("/members")
+	}
 	const member = createAsync(() => getMember(Number(params.id)));
 
   return (
