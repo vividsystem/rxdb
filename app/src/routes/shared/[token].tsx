a71743f5e5892f1ba3e1c23711a0319b29e8828a
vividsystem <notrealbutnotobot@gmail.com>
2025-07-30 09:07:47 +0200

    chore: removed share link route

--- app/src/routes/shared/[token].tsx
+++
@@ -1,8 +0,0 @@
-import { query } from "@solidjs/router";
-import { NewShareToken } from "~/api/server";
-
-export default function SharedMember() {
-	const getShare = query(async (token: NewShareToken) => {
-
-	}, "getShare")
-}
