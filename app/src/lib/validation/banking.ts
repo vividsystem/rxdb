2c05c20a81cc84214fa1491919f38e188e206d5d
vividsystem <notrealbutnotobot@gmail.com>
2025-07-30 09:03:15 +0200

    feat: implemented validation for bankingInfo

---
+++ app/src/lib/validation/banking.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+import { defaultString } from ".";
+
+
+export const bankingId = z.number().positive()
+export const bankingSchema = z.object({
+	id: bankingId,
+	firstname: defaultString,
+	lastname: defaultString,
+	IBAN: z.string().trim().min(1).max(33),
+	BIC: z.string().trim().max(11).optional(),
+}).strict()
+
+
+export const createBankingSchema = bankingSchema.omit({ id: true }).strip()
+
+
+export const updateBankingSchema = createBankingSchema.partial().strip()
