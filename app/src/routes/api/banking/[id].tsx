e65e0bad04810b2e7f5e73212320fcd5eb4672b6
vividsystem <notrealbutnotobot@gmail.com>
2025-07-30 09:05:27 +0200

    feat: added banking info api endpoints

---
+++ app/src/routes/api/banking/[id].tsx
@@ -0,0 +1,68 @@
+import { bankingInfo } from "@/schema";
+import { json } from "@solidjs/router";
+import type { APIEvent } from "@solidjs/start/server";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { db } from "~/lib/db";
+import { bankingId, createBankingSchema } from "~/lib/validation/banking";
+
+export async function GET({ params }: APIEvent) {
+	const paramsParser = bankingId.safeParse(Number(params.id))
+	if(!paramsParser.success) {
+    return json({ errors: z.formatError(paramsParser.error) }, { status: 400 });
+	}
+
+	const banking = await db.select().from(bankingInfo).where(eq(bankingInfo.id, paramsParser.data))
+	if(banking.length < 1) {
+		return json({message: "Not found"}, {status: 404})
+	}
+
+	return json({ banking: banking[0] }, { status: 200 })
+
+}
+
+export async function POST({ request }: APIEvent) {
+	const body = await request.json()
+	const bodyParser = createBankingSchema.safeParse(body)
+	if(!bodyParser.success) {
+    return json({ errors: z.formatError(bodyParser.error) }, { status: 400 });
+	}
+	
+	const banking = await db.insert(bankingInfo).values(bodyParser.data).returning()
+
+	return json({ banking: banking[0] }, { status: 201 })	
+}
+
+export async function UPDATE({ request, params }: APIEvent) {
+	const paramsParser = bankingId.safeParse(Number(params.id))
+	if(!paramsParser.success) {
+    return json({ errors: z.formatError(paramsParser.error) }, { status: 400 });
+	}
+
+	const body = await request.json()
+	const bodyParser = createBankingSchema.safeParse(body)
+	if(!bodyParser.success) {
+    return json({ errors: z.formatError(bodyParser.error) }, { status: 400 });
+	}
+
+	const banking = await db.update(bankingInfo).set(bodyParser.data).where(eq(bankingInfo, paramsParser.data))		
+	if(banking.length < 1) {
+		return json({message: "Not found"}, {status: 404})
+	}
+
+	return json({ banking: banking[0]}, { status: 200})
+}
+
+export async function DELETE({ request, params }: APIEvent) {
+	const paramsParser = bankingId.safeParse(Number(params.id))
+	if(!paramsParser.success) {
+    return json({ errors: z.formatError(paramsParser.error) }, { status: 400 });
+	}
+
+	const banking = await db.delete(bankingInfo).where(eq(bankingInfo.id, paramsParser.data))
+	if(banking.length < 1) {
+		return json({message: "Not found"}, {status: 404})
+	}
+
+	return json({ banking: banking[0]}, { status: 200})
+}
