dd133dfaeb3661a529c35c70225fdc9cbe353f39
vividsystem <notrealbutnotobot@gmail.com>
2025-08-10 13:30:33 +0200

    feat: beautified login

--- app/src/routes/login.tsx
+++ app/src/routes/login.tsx
@@ -1,4 +1,3 @@
-import { action, redirect, RouteDefinition } from "@solidjs/router";
 import { createEffect, createSignal, onMount } from "solid-js";
 import { authClient } from "~/lib/auth-client";
 import Input from "~/components/Input";
@@ -23,16 +22,18 @@ export default function Login() {
 	}
 
   return (
-    <main class="w-full h-screen p-4 space-y-2 flex flex-col items-center">
-			Login
-			<form>
-				<Input type="email" name="email" placeholder="name@example.com" value={email} setValue={setEmail} label={() => "Email"} />
-				<button class="text-xl" onClick={async (e) => {
-					e.preventDefault()
-					const err = login(email())
-					console.log(err)
-				}}>Login</button>
-			</form>
+    <main class="w-full h-screen p-4 space-y-2 flex justify-center items-center">
+			<div class="container">
+				<h1 class="text-2xl">Login or sign-up</h1>
+				<form class="flex flex-col jusitfy-start gap-4">
+					<Input type="email" name="email" placeholder="name@example.com" value={email} setValue={setEmail} label={() => "Email"} />
+					<button class="w-fit btn text-2xl" onClick={async (e) => {
+						e.preventDefault()
+						const err = login(email())
+						console.log(err)
+					}}>Login</button>
+				</form>
+			</div>
     </main>
   );
 }
