61ee00525a8b580e8f5596f9c59ab7daf15f5e82
vividsystem <notrealbutnotobot@gmail.com>
2025-08-14 19:56:01 +0200

    feat: added route for currently logged in member

--- app/src/routes/api/member.tsx
+++ app/src/routes/api/member.tsx
@@ -4,6 +4,7 @@ import z from "zod";
 import { db } from "~/lib/db";
 import { createMemberInput } from "~/lib/validation/member";
 import type { APIEvent } from "@solidjs/start/server";
+import { requireUser } from "~/lib/auth";
 
 export async function POST({ request }: APIEvent) {
 	const body = await request.json();
@@ -16,4 +17,13 @@ export async function POST({ request }: APIEvent) {
 	const member = await db.insert(members).values(result.data).returning()
 
 	return json({ member: member[0] }, { status: 201 })
+}
+
+export async function GET(event: APIEvent) {
+	const loggedInUser = await requireUser(event)
+	if(!loggedInUser) {
+		return json({ message: "Unauthorized" }, { status: 401 })
+	}
+
+	return json({ member: loggedInUser.member})
 }
