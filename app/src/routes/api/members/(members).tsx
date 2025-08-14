6314b6be37f747ebb94e390779b5a30a4e07317e
vividsystem <notrealbutnotobot@gmail.com>
2025-08-14 19:57:14 +0200

    feat: added rbac to members route

--- app/src/routes/api/members/(members).tsx
+++ app/src/routes/api/members/(members).tsx
@@ -2,9 +2,18 @@ import { members } from "../../../../drizzle/schema";
 import { json } from "@solidjs/router";
 import { eq } from "drizzle-orm";
 import { db } from "~/lib/db";
+import type { APIEvent } from "@solidjs/start/server";
+import { requireUser } from "~/lib/auth";
+import { hasPermission } from "~/lib/auth/roles";
 
-export async function GET() {
-
+export async function GET(event: APIEvent) {
+	const loggedIn = await requireUser(event)
+	// user logged in and with perms
+	if(!loggedIn 
+		|| !hasPermission(loggedIn.member.id, "view_members")) {
+		return json({ message: "Unauthorized" }, { status: 401 })
+	}
+	
 	const allMembers = await db.select().from(members).where(eq(members.verified, true)).catch((e) => {
 		throw e
 	});
--- app/src/routes/api/members/pending.tsx
+++ app/src/routes/api/members/pending.tsx
@@ -1,9 +1,18 @@
 import { members } from "../../../../drizzle/schema";
 import { json } from "@solidjs/router";
 import { eq } from "drizzle-orm";
+import { requireUser } from "~/lib/auth";
 import { db } from "~/lib/db";
+import type { APIEvent } from "@solidjs/start/server";
+import { hasPermission } from "~/lib/auth/roles";
 
-export async function GET() {
+export async function GET(event: APIEvent) {
+	const loggedIn = await requireUser(event)
+	// user logged in and with perms
+	if(!loggedIn 
+		|| !hasPermission(loggedIn.member.id, "view_members")) {
+		return json({ message: "Unauthorized" }, { status: 401 })
+	}
 
 	const allMembers = await db.select().from(members).where(eq(members.verified, false)).catch((e) => {
 		throw e
