801a5ccfbe0b404673571e40202d1a5df3dfd423
vividsystem <notrealbutnotobot@gmail.com>
2025-08-20 14:28:25 +0200

    fix: updated auth client to match backend auth

--- app/src/lib/auth-client.ts
+++ app/src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/client";
-import type { auth } from "./auth.ts";
-import { inferAdditionalFields, magicLinkClient } from "better-auth/client/plugins";
+import { magicLinkClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
     baseURL: "http://localhost:3000",
