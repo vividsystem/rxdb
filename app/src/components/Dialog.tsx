cc994f457725cf826e7e41f7e26f92845326c557
vividsystem <notrealbutnotobot@gmail.com>
2025-09-06 01:08:58 +0200

    fix: changes z-layer of dialog to not interfere with alerts

--- app/src/components/Dialog.tsx
+++ app/src/components/Dialog.tsx
@@ -13,9 +13,9 @@ export default function Dialog(props: ParentProps<DialogProps>) {
 				{props.trigger}
 			</KobalteDialog.Trigger>
 			<KobalteDialog.Portal>
-				<KobalteDialog.Overlay class="fixed z-50 top-0 bottom-0 left-0 right-0 backdrop-blur-md"/>
+				<KobalteDialog.Overlay class="fixed z-40 top-0 bottom-0 left-0 right-0 backdrop-blur-md"/>
 
-				<div class="fixed z-50 inset-0 flex justify-center items-center">
+				<div class="fixed z-40 inset-0 flex justify-center items-center">
 				<KobalteDialog.Content>
 							<div class="isolate relative container">
 							<KobalteDialog.CloseButton class="absolute right-0 top-0 p-4">
