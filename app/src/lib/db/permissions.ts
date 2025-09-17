640d8429a8f2c37495ce552535c65b2ed61e55f2
vividsystem <notrealbutnotobot@gmail.com>
2025-09-17 16:40:27 +0200

    feat: added permissions for roles and permissions

--- app/src/lib/db/permissions.ts
+++ app/src/lib/db/permissions.ts
@@ -3,9 +3,12 @@ export const PERMISSIONS = [
   "edit_members",
   "delete_members",
   "assign_roles",
+	"edit_roles",
+	"view_roles",
   "view_banking",
   "edit_banking",
   "delete_banking",
+	"view_permissions", // Is this neccessary? -> view_roles does pretty much the same
 	
 ] as const;
 
